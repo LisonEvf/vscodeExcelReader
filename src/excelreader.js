830167ad51f4efba8476d319536808671ddab65a
lisonevf <lisonevf@gmail.com>
2022-03-01 23:31:40 +0800

    20220301

--- src/excelreader.js
+++ src/excelreader.js
@@ -4,12 +4,19 @@ const provider = require('./provider/excelreaderprovider')
 
 module.exports = function(context) {
     context.subscriptions.push(
-        vscode.window.registerCustomEditorProvider('excelreader.erv'),
-        new provider(),
+        vscode.window.registerCustomEditorProvider('excelreader.erv',
+        new provider(context),
         {
             webviewOptions: { 
                 retainContextWhenHidden: true, 
                 enableFindWidget: true 
             }
         })
+    )
+
+    context.subscriptions.push(
+        vscode.window.registerTreeDataProvider('excelreaderview',function(){
+
+        })
+    )
 }
--- src/provider/excelreaderprovider.js
+++ src/provider/excelreaderprovider.js
@@ -1,1 +1,16 @@
 const vscode = require('vscode')
+
+ class erp {
+    constructor(context){
+        this.extensionPath = context.extensionPath;
+    }
+
+    openCustomDocument(uri,context,token){
+        return {uri,function(){}}
+    }
+
+    resolveCustomEditor(doc,webviewpanel,token){
+        webviewpanel.webview.html = 'yes'
+    }
+}
+module.exports = erp
